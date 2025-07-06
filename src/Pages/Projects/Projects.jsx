898bb5e730d572627fe3a2ab82ee83c4b34758e4
Harsh Chauhan <chauhanharsh388@gmail.com>
2025-07-06 21:59:15 +0530

    Projects Section addes with full responsiveness and all the details

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import "./Projects.css";
 import { motion } from "framer-motion";
 import ScrollHashUpdater from "../../Components/ScrollHashUpdater/ScrollHashUpdater";
+import QuickCart from "../../assets/QuickCart.png";
+import TwinsAI from "../../assets/TwinsAI.png";
+import Portfolio from "../../assets/Portfolio.png";
 
 
 const projectsData = [
   {
-    title: "Project One",
-    description: "A brief description of the project and what it achieves.",
-    link: "https://github.com/yourusername/project-one",
+    image: Portfolio,
+    title: "Portfolio",
+    description: "A fully responsive portfolio site built with React.js to showcase my projects, skills, and experience. Designed with a clean and modern UI, smooth scroll navigation, and section-based layout. Emphasized performance, accessibility, and clear visual hierarchy to ensure an engaging user experience across devices.",
+    link: "http://vite-portfolio-psi.vercel.app/",
   },
   {
-    title: "Project Two",
-    description: "An overview of the second project with its features.",
-    link: "https://github.com/yourusername/project-two",
+    image: QuickCart,
+    title: "QuickCart",
+    description: "A sleek and responsive e-commerce website built with React.js and Tailwind CSS. Features secure API integrations for product browsing, cart management, and user authentication. Designed for a seamless and intuitive shopping experience with modern UI and smooth navigation.",
+    link: "https://quick-cart-three.vercel.app/",
   },
   {
-    title: "Project Three",
-    description: "Details about the third project and its purpose.",
-    link: "https://github.com/yourusername/project-three",
+    image: TwinsAI,
+    title: "TwinsAI",
+    description: "A responsive and accessible chatbot interface inspired by Google's Gemini, built with React.js. It integrates real-time API communication to deliver instant replies while maintaining a clean, minimalistic user experience. Designed with performance and usability in mind.",
+    link: "https://twins-ai.vercel.app/",
   },
 ];
 
@@ -41,10 +47,11 @@ const Projects = () => {
             whileHover={{ scale: 1.03 }}
             transition={{ type: "spring", stiffness: 300 }}
           >
-            <h3>{project.title}</h3>
+            <img src={project.image} alt="Project Image" />
+            <h2>{project.title}</h2>
             <p>{project.description}</p>
             <button onClick={() => window.open(project.link, "_blank")}>
-              View Project
+              View Live
             </button>
           </motion.div>
         ))}
